f7b2cb5e6e7990d810d27ecd25fd4311358c059a
DataRohit <rohit.vilas.ingole@gmail.com>
2024-04-08 21:41:29 +0530

    Verce Settings Updated

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -107,7 +107,7 @@ STATIC_URL = "/static/"
 
 
 # Set the static files root
-STATIC_ROOT = BASE_DIR.parent / "staticfiles" / "static"
+STATIC_ROOT = BASE_DIR.parent / "staticfiles_build"
 
 
 # # Set the static files directories
